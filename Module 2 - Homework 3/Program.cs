c4ac4bb8b52bd0440a79cbbe9923960d43ca81f4
haci1994 <65703054+haci1994@users.noreply.github.com>
2025-02-28 23:43:56 +0400

    missing commitment

---
+++ Module 2 - Homework 3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Module_2___Homework_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a text to count all vowels in:");
+            string text = Console.ReadLine();
+
+            text.VowelCount();
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static void VowelCount(this string text)
+        {
+            string chekingText = text.ToLower();
+            int _count = 0;
+
+            for (int i = 0; i < chekingText.Length; i++)
+            { 
+                if (chekingText[i] == 'a' || chekingText[i] == 'e' || chekingText[i] == 'o' || chekingText[i] == 'i' || chekingText[i] == 'u')
+                {
+                    _count++;
+                }
+            }
+
+            Console.WriteLine($"Number of vowels in '{text}': {_count}");
+        }
+    }
+}
